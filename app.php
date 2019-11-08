d1761f07d475f62cf90bd21a54f7f1179966f4fd
Jonah Karew <jonahkarew@gmail.com>
2019-11-08 00:38:58 -0500

    proper template formatting

---
+++ app.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="./app.php"></script>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h1>Here is the main part</h1>
+    <h3><?php echo $text; ?></h3>
+    <h4>I have tried this: <?php echo $randomNum?>   times</h4>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,35 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $title ?></title>
-</head>
-<body>
-    <h1>Here is the main part</h1>
+<?php
+$title = 'teenage mutant ninja turtle forum';
+$text = 'this is that text';
+$randomNum = random_int(1, 20);
 
-
-
-
-
-
-
-
-
-
-
-    <?php
-
-    $title = 'Home';
-    $text = 'this is that text';
-    $randomNum = random_int(1, 20);
-    // echo 'how did i get here. ';
-
-
-    // echo $text;
-
-    // echo $randomNum
-    ?>
-</body>
-</html>
+include 'app.php';
+?>
